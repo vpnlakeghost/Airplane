d7596014563765df76aed5f24fd05a24f3f684ee
vpnlakeghost <99Wvu.gRfge>
2022-11-15 15:37:01 +0100

    progress to save

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,3 +1,14 @@
 public class Airplane {
 
+    private int id;
+
+    private boolean landed;
+
+    public void takeOff() {
+        landed = false;
+    }
+
+    public void landed() {
+        landed = true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Airplane plane1 = new Airplane();
+        Airplane plane2 = plane1;
+        Airplane plane3 = new Airplane();
 
     }
 }
